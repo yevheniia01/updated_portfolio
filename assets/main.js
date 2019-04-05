a69e40f5a7935f50947ea2b38d4fb36a353b9ccb
yevheniia <jene4ka1818@gmail.com>
2019-04-05 12:51:38 -0400

    update3

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
    $('.contactMe').on('click', function(){
 
     $('#contactModal').fadeIn()
-    
+    $('body').append('<div class = "modal-backdrop"></div>')
    })
   $('.cl').on('click', function(){
       $('#contactModal').fadeOut();
+      $('.modal-backdrop').remove();
   })
 
 
