32bb1dba2ab4da8d5235fc7d457c65113889e647
yevheniia <jene4ka1818@gmail.com>
2019-04-05 12:36:01 -0400

    update2

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.contactMe').on('click', function(){
+
+    $('#contactModal').fadeIn()
+    
+   })
+  $('.cl').on('click', function(){
+      $('#contactModal').fadeOut();
+  })
+
+
+})
